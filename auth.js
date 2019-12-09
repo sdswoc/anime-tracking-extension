fa2963b41470c68a1e2abcd425b472c1994544a6
dandraag <agarwal.kush17181@gmail.com>
2019-12-09 22:21:22 +0530

    Put script in a different file

---
+++ auth.js
@@ -0,0 +1,19 @@
+window.onload = function(){
+    document.getElementById("auth").onclick = 
+    function authorise(){
+        chrome.identity.launchWebAuthFlow(
+        {
+            'url': 'https://anilist.co/api/v2/oauth/authorize?client_id=2636&response_type=token',
+            'interactive': true
+        },
+        function(url){
+            console.log(url);
+            var start = url.search("=");
+            var fin = url.search("&",start);
+            var token = url.substring(start+1,fin);
+            chrome.storage.sync.set({code: token}, function() {
+                console.log('Code is set to ' + token);
+            });
+        });
+    }
+}
