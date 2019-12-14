cbabfdfcba4952a995b8b4bae645ef0c8d518613
dandraag <agarwal.kush17181@gmail.com>
2019-12-14 23:02:43 +0530

    implemented popup.js

--- content.js
+++ content.js
@@ -1,12 +1,10 @@
-var id,eps;
-
 var path = window.location.pathname;
 console.log(path);
 var namend = path.indexOf('/',3);
 eps = path.substring(namend+1);
 var name = path.substring(3,namend);
 
-chrome.storage.sync.set({eps_no: eps}, function() {
+/*chrome.storage.sync.set({eps_no: eps}, function() {
     console.log('Episode number is set to ' + eps);
 });
     
@@ -15,22 +13,28 @@ setInterval( function (){
     chrome.storage.sync.set({anime_width: w}, function() {
         console.log('Anime width is set to ' + w);
     });
-} , 1000);
+} , 1000);*/
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        switch(request.req){
+            case "anime-eps":
+                sendResponse({resp: eps});
+                console.log("sent "+eps);
+                break;
+        }
+});
 
 var query = `
-query ($id: Int, $page: Int, $perPage: Int, $search: String) {
-    Page (page: $page, perPage: $perPage) {
-        media (id: $id, search: $search) {
-            id
-       }
+query ($id: Int, $search: String) {
+    Media (id:$id, search: $search) {
+        id
     }
 }
 `;
 
 var variables = {
     search: name,
-    page: 1,
-    perPage: 1
 };
 
 var url = 'https://graphql.anilist.co',
@@ -49,9 +53,18 @@ var url = 'https://graphql.anilist.co',
 fetch(url, options).then((res)=>res.json())
 .then((data)=>{
     console.log(data);
-    id = data.data.Page.media[0].id;
+    id = data.data.Media.id;
     console.log(id+" "+ eps);
-    chrome.storage.sync.set({anime_id: id}, function() {
+    /*chrome.storage.sync.set({anime_id: id}, function() {
         console.log('Anime ID is set to ' + id);
+    });*/
+    chrome.runtime.onMessage.addListener(
+        function(request, sender, sendResponse) {
+            switch(request.req){
+                case "anime-id":
+                    sendResponse({resp: id});
+                    console.log("sent id");
+                    break;
+            }
     });
 });
