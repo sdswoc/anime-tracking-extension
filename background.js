3d561bd50d70ef245d20c16c462aa66b696a01e6
dandraag <agarwal.kush17181@gmail.com>
2019-12-19 12:47:13 +0530

    made background page check anime id and eps, content page check for width

--- background.js
+++ background.js
@@ -9,3 +9,54 @@ chrome.runtime.onInstalled.addListener(function() {
       }]);
     });
 });
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    var el = document.createElement('a');
+    el.href = changeInfo.url;
+    if(el.hostname=="twist.moe"&&el.pathname!="/"){
+        var path = el.pathname;
+        console.log(path);
+        var namend = path.indexOf('/',3);
+        var eps = path.substring(namend+1);
+        chrome.storage.local.set({ep_no: eps}, function() {
+          console.log('Episode number is set to ' + eps);
+        });
+
+
+        var name = path.substring(3,namend);
+        var query = `
+        query ($id: Int, $search: String) {
+            Media (id:$id, search: $search) {
+                id
+           }
+        }
+        `;
+
+        var variables = {
+            search: name,
+        };
+
+        var url = 'https://graphql.anilist.co',
+            options = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                },
+                body: JSON.stringify({
+                    query: query,
+                    variables: variables
+                })
+            };
+
+        fetch(url, options).then((res)=>res.json())
+        .then((data)=>{
+            console.log(data);
+            id = data.data.Media.id;
+            chrome.storage.local.set({anime_id: id}, function() {
+                console.log('Anime ID is set to ' + id);
+            });
+        });
+
+    }
+});
