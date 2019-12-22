ee35a12bb23d537ea1a7b5fe0239f0ebf668782f
dandraag <agarwal.kush17181@gmail.com>
2019-12-22 22:49:58 +0530

    added storage functionality to options page?

--- options.js
+++ options.js
@@ -1,3 +1,16 @@
+chrome.storage.local.set({percent: 90}, function() {});
+chrome.storage.local.get(['percent'], function(result){
+    console.log(result);
+    var per = result.percent;
+    document.getElementById("getper").placeholder = per;
+});
+
 document.getElementById("btn").onclick = function(){
-    alert(document.getElementById("getper").value);
+    var per = document.getElementById("getper").value;
+    if(per == ""){
+        per = document.getElementById("getper").placeholder;
+    }
+    chrome.storage.local.set({percent: per}, function() {
+        console.log("Percentage for change set to " + per);
+    });
 }
